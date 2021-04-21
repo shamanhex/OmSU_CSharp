94b0cb7eb5dc06bdf58bc84443e7b8ab7bd608cd
Vladimir Shishkin <shaman.hex@gmail.com>
2021-04-21 11:14:24 +0600

    Implement autoresize images

--- Examples/ImgUtils/AutoScale/Program.cs
+++ Examples/ImgUtils/AutoScale/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CmdLineUtils;
+using ImgUtils;
 
 namespace AutoScale
 {
@@ -18,6 +22,52 @@ namespace AutoScale
             Console.WriteLine("Output path: {0}", cmdLineParams.OutputPath);
             Console.WriteLine("Max width:   {0}", cmdLineParams.MaxWidth);
             Console.WriteLine("Max height:  {0}", cmdLineParams.MaxHeight);
+
+            if (File.Exists(cmdLineParams.InputPath))
+            {
+                ProcessFile(cmdLineParams.InputPath, cmdLineParams.OutputPath, cmdLineParams.MaxWidth, cmdLineParams.MaxHeight);
+            }
+            else if (Directory.Exists(cmdLineParams.InputPath))
+            {
+                ProcessDirectory(cmdLineParams.InputPath, cmdLineParams.OutputPath, cmdLineParams.MaxWidth, cmdLineParams.MaxHeight);
+            }
+            else
+            {
+                throw new InvalidOperationException("Path access denied: " + cmdLineParams.InputPath);
+            }
+            
+        }
+
+        public static readonly string[] IMAGE_EXTENSIONS = new string[] { ".jpg", ".png", ".bmp" };
+
+        private static void ProcessDirectory(string inputPath, string outputPath, int maxWidth, int maxHeight)
+        {
+            string[] inputFiles = Directory.GetFiles(inputPath);
+            foreach (string inputFile in inputFiles)
+            {
+                string fileExt = Path.GetExtension(inputFile).ToLower();
+                if (!IMAGE_EXTENSIONS.Contains(fileExt))
+                {
+                    Console.WriteLine(string.Format("File type not supported ({0}): {1}", fileExt, inputFile));
+                    continue;
+                }
+                string outputFile = Path.Combine(outputPath, Path.GetFileName(inputFile));
+                ProcessFile(inputFile, outputFile, maxWidth, maxHeight);
+            }
+        }
+
+        private static void ProcessFile(string inputPath, string outputPath, int maxWidth, int maxHeight)
+        {
+            Bitmap fileImg = (Bitmap) Image.FromFile(inputPath);
+            Bitmap destImg = ImageScaler.Resize(fileImg, maxWidth, maxHeight);
+            string folderPath = Path.GetDirectoryName(Path.GetFullPath(outputPath));
+            if (!Directory.Exists(folderPath))
+            {
+                Console.WriteLine(string.Format("Create direcotry: {0}", folderPath));
+                Directory.CreateDirectory(folderPath);
+            }
+            Console.WriteLine(string.Format("File processed: {0}", outputPath));
+            destImg.Save(outputPath);
         }
     }
 }
---
+++ Examples/ImgUtils/ImgUtils/ImageScaler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgUtils
+{
+    public static class ImageScaler
+    {
+        public static Bitmap Resize(Bitmap img, int maxWidth, int maxHeight)
+        {
+            double k = GetResizeCoeff(img.Width, img.Height, maxHeight, maxHeight);
+            
+            int destWidth = (int) (img.Width * k);
+            int destHeight = (int) (img.Height * k);
+            
+            Rectangle destRect = new Rectangle(0, 0, destWidth, destHeight);
+            Bitmap destImg = new Bitmap(destWidth, destHeight);
+            destImg.SetResolution(img.HorizontalResolution, img.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImg))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(img, destRect, 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImg;
+        }
+
+        public static double GetResizeCoeff(int width, int height, int maxWidth, int maxHeight)
+        {
+            double kWidth = maxWidth / (double)width;
+            double kHeight = maxHeight / (double)height;
+            return Math.Min(kWidth, kHeight);
+        }
+    }
+
+}
